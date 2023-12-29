b0727c6554108bad646c609656498b6aa7f52104
Kijanaa <irene.yanapouth@gmail.com>
2023-12-30 00:40:59 +0100

    commit

--- App.js
+++ App.js
@@ -5,19 +5,17 @@ import { TailwindProvider } from 'tailwindcss-react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 
-
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <TailwindProvider>
-         <StackNavigator>
-         <Stack.Screen name="Home" component={HomeScreen} />
-         </StackNavigator>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
       </TailwindProvider>
     </NavigationContainer>
-   
   );
 }
 
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,15 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useLayoutEffect } from 'react'
 
 const HomeScreen = () => {
+    const navigation = useNavigation();
+
+    useLayoutEffect(() => {
+        navigation.setOptions ({
+            headerTitle : 'TESTING'
+         )}
+    }, [])
+
   return (
     <View>
       <Text className="text-red-700">HomeScreen</Text>

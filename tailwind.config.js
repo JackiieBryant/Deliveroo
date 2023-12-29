d5f10be68c0c373ded35d8a562530180676b04c5
Kijanaa <irene.yanapouth@gmail.com>
2023-12-29 04:40:37 +0100

    second

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./screens/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  // ...
+};
+
